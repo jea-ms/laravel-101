a55bdf21b025167ac51e7f8c1e94345f563a016e
Jea Suson <susonjea@gmail.com>
2023-05-16 22:40:29 +0800

    fetch from api products

--- client/components/Products.jsx
+++ client/components/Products.jsx
@@ -3,44 +3,13 @@
 import { useState, useEffect } from 'react'
 import ProductCard from './ProductCard'
 
-// const productsArray = []
-const productsArray = [
-    {
-        "id": 1,
-        "name": "Product One",
-        "slug": "product-one",
-        "description": "This is product one",
-        "price": "9.99",
-        "created_at": "2023-05-12T02:20:19.000000Z",
-        "updated_at": "2023-05-12T02:20:19.000000Z"
-    },
-    {
-        "id": 2,
-        "name": "Product Two",
-        "slug": "product-two",
-        "description": "This is product two",
-        "price": "2.99",
-        "created_at": "2023-05-12T02:29:39.000000Z",
-        "updated_at": "2023-05-12T02:37:11.000000Z"
-    },
-    {
-        "id": 4,
-        "name": "Iphone 12",
-        "slug": "iphone-12",
-        "description": "This is Iphone 12",
-        "price": "51.99",
-        "created_at": "2023-05-12T02:45:17.000000Z",
-        "updated_at": "2023-05-12T02:45:17.000000Z"
-    }
-]
-
 const ProductList = ({ data }) => {
     return (
         <div>
             {
                 data.length > 0 ? (
                     data.map((prod) => (
-                        <ProductCard product={prod}/>
+                        <ProductCard product={prod} />
                     ))
                 ) : (
                     <span>No products found</span>
@@ -55,11 +24,10 @@ const Products = () => {
 
     useEffect(() => {
         const fetchProducts = async () => {
-            // TODO: add get all products
-            // const response = await fetch('/api/prompt')
-            // const data = await response.json()
+            const res = await fetch(`http://127.0.0.1:8000/api/products/`)
+            const data = await res.json()
 
-            setProducts(productsArray)
+            setProducts(data)
         }
 
         fetchProducts()
