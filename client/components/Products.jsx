6bdf5ef5f8e6aa847450399779bb810b5b73d6f0
Jea Suson <susonjea@gmail.com>
2023-05-16 20:42:38 +0800

    get product list

---
+++ client/components/Products.jsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+// const productsArray = []
+const productsArray = [
+    {
+        "id": 1,
+        "name": "Product One",
+        "slug": "product-one",
+        "description": "This is product one",
+        "price": "9.99",
+        "created_at": "2023-05-12T02:20:19.000000Z",
+        "updated_at": "2023-05-12T02:20:19.000000Z"
+    },
+    {
+        "id": 2,
+        "name": "Product Two",
+        "slug": "product-two",
+        "description": "This is product two",
+        "price": "2.99",
+        "created_at": "2023-05-12T02:29:39.000000Z",
+        "updated_at": "2023-05-12T02:37:11.000000Z"
+    },
+    {
+        "id": 4,
+        "name": "Iphone 12",
+        "slug": "iphone-12",
+        "description": "This is Iphone 12",
+        "price": "51.99",
+        "created_at": "2023-05-12T02:45:17.000000Z",
+        "updated_at": "2023-05-12T02:45:17.000000Z"
+    }
+]
+
+const ProductList = ({ data }) => {
+    return (
+        <div className='container flex flex-col items-center justify-center mx-auto rounded-lg'>
+            {
+                data.length > 0 ? (
+                    data.map((prod) => (
+                        <div>
+                            <span>{prod.name}</span>
+                            <br />
+                        </div>
+                    ))
+                ) : (
+                    <span>No products found</span>
+                )
+            }
+        </div>
+    )
+}
+
+const Products = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            // TODO: add get all products
+            // const response = await fetch('/api/prompt')
+            // const data = await response.json()
+
+            setProducts(productsArray)
+        }
+
+        fetchProducts()
+    })
+
+    return (
+        <div className="border_div">
+            <ProductList data={products} />
+        </div>
+
+    )
+}
+
+export default Products
