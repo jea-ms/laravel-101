6fa48652f62ab9d45bfb2cbff59798ac7c8b97c5
Jea Suson <susonjea@gmail.com>
2023-05-17 13:11:58 +0800

    ok edit product

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import Products from "@components/Products"
 import TitlePage from "@components/TitlePage"
 
 const Home = () => {
+    const router = useRouter()
     const [products, setProducts] = useState([])
 
 useEffect(() => {
@@ -19,7 +21,7 @@ useEffect(() => {
 }, [])
 
 const editProduct = async (id) => {
-
+    router.push(`/products/update?id=${id}`)
     console.log('edit', id)
 }
 
--- client/app/products/update/page.jsx
+++ client/app/products/update/page.jsx
@@ -1,43 +1,59 @@
 'use client'
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import Form from "@components/Form"
 import TitlePage from "@components/TitlePage"
 
 
 const UpdateProduct = () => {
-    const router = useRouter()
-    const [submitting, setSubmitting] = useState(false)
-    const [product, setProduct] = useState({
-        name: '',
-        slug: '',
-        description: '',
-        price: '',
-    })
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const prodId = searchParams.get('id')
 
-    const editProduct = async (e) => {
-        e.preventDefault()
-        setSubmitting(true)
+  const [submitting, setSubmitting] = useState(false)
+  const [product, setProduct] = useState({
+    name: '',
+    slug: '',
+    description: '',
+    price: '',
+  })
 
-        try {
-            const response = await fetch(`http://localhost:8000/api/products/${id}`, {
-                method: 'PUT',
-                headers: {
-                  'Content-type': 'application/json'
-                },
-                body: JSON.stringify(product)
-            })
+  useEffect(() => {
+    const getProdDetails = async () => {
+      const res = await fetch(`http://localhost:8000/api/products/${prodId}`)
+      const data = await res.json()
 
-            if (response.ok) {
-                router.push('/')
-            }
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setSubmitting(false)
-        }
+      setProduct(data)
     }
+
+    if (prodId) getProdDetails()
+  },[prodId])
+
+  const editProduct = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    if(!prodId) return alert('Product not found')
+
+    try {
+      const response = await fetch(`http://localhost:8000/api/products/${prodId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      })
+
+      if (response.ok) {
+        router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
+  }
   return (
     <div className="grr max-w-7xl pt-20 mx-auto text-center">
       <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
@@ -46,11 +62,11 @@ const UpdateProduct = () => {
           showBtnAdd={false} />
         <div className='object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md'>
           <Form
-          type='Add'
-          product={product}
-          setProduct={setProduct}
-          submitting={submitting}
-          onSubmit={editProduct}
+            type='Edit'
+            product={product}
+            setProduct={setProduct}
+            submitting={submitting}
+            onSubmit={editProduct}
           />
         </div>
       </div>
