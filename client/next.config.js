c45f3006a41efd305af7f9bf739c3468cfb0db7c
Jea Suson <susonjea@gmail.com>
2023-05-17 17:16:17 +0800

    add env on config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_PRODUCT_URL: 'http://localhost:8000/api/products/',
+    }
+}
 
 module.exports = nextConfig
