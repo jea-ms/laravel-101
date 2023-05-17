88b20c6aa14b58c057446bf28f386c7de9f31ffb
Jea Suson <susonjea@gmail.com>
2023-05-17 09:54:42 +0800

    add form

--- client/app/products/new/page.jsx
+++ client/app/products/new/page.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+'use client'
 
-const NewPrompt = () => {
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import Form from "@components/Form"
+import TitlePage from "@components/TitlePage"
+
+const NewProduct = () => {
+    const router = useRouter()
+    const [submitting, setSubmitting] = useState(false)
+    const [product, setProduct] = useState({
+        name: '',
+        slug: '',
+        description: '',
+        price: '',
+    })
+
+    const createProduct = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if (response.ok) {
+                router.push('/')
+            }
+
+            router.push('/')
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
   return (
-    <div>NewPrompt</div>
+    <div className="grr max-w-7xl pt-20 mx-auto text-center">
+      <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
+        <TitlePage
+          title='Add Product'
+          showBtnAdd={false} />
+        <div className='object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md'>
+          <Form
+          type='Add'
+          product={product}
+          setProduct={setProduct}
+          submitting={submitting}
+          onSubmit={createProduct}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default NewPrompt
+export default NewProduct
--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,7 +1,56 @@
-const Form = ({ type, product, setProduct, submitting, onSubmit}) => {
-  return (
-    <div>Form</div>
-  )
+const Form = ({ type, product, setProduct, submitting, onSubmit }) => {
+    return (
+        <form className="add-form" onSubmit={onSubmit}>
+            <div className="form-control">
+                <label><span>Name</span></label>
+                <input
+                    type='text'
+                    value={product.name}
+                    onChange={(e) => setProduct({ ...product,
+                        name: e.target.value })}
+                    placeholder='name'
+                    required
+                />
+            </div>
+            <div className="form-control">
+                <label><span>Slug</span></label>
+                <input
+                    type='text'
+                    value={product.slug}
+                    onChange={(e) => setProduct({ ...product,
+                        slug: e.target.value })}
+                    placeholder='slug'
+                    required
+                />
+            </div>
+            <div className="form-control">
+                <label><span>Description</span></label>
+                <textarea
+                    value={product.description}
+                    onChange={(e) => setProduct({ ...product,
+                        description: e.target.value })}
+                    placeholder='Describe your product'
+                    required
+                    className='form_textarea'
+                />
+            </div>
+           
+            <div className="form-control">
+                <label><span>Price</span></label>
+                <input
+                    type='number'
+                    value={product.price}
+                    onChange={(e) => setProduct({ ...product,
+                        price: e.target.value })}
+                    placeholder='0.00'
+                    required
+                />
+            </div>
+
+            <input type='submit' value={type} className='btn btn-block' />
+            
+        </form>
+    )
 }
 
 export default Form
