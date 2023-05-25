593abf0165ee933e9b68f9737f02fa7f1ab5ccf4
Jea Suson <susonjea@gmail.com>
2023-05-25 13:51:18 +0800

    add route login

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,14 @@
 'use client'
 
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+    const router = useRouter()
+
+    const loginBtn = () => {
+        router.push(`/products/new`)
+    }
     return (
         <div className="fixed top-0 w-full z-30 clearNav md:bg-opacity-90 transition duration-300 ease-in-out">
             <div className="flex flex-col max-w-6xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
@@ -17,13 +21,13 @@ const Header = () => {
                             Company Inc.
                         </h1>
                     </Link>
-                    <button className='md:hidden black_btn'>
+                    <button onClick={loginBtn} className='md:hidden black_btn'>
                         Sign in
                     </button>
 
                 </div>
                 <div className="md:flex hidden flex flex-grow justify-end flex-wrap items-center">
-                    <button className='black_btn'>
+                    <button onClick={loginBtn} className='black_btn'>
                         Sign in </button>
                 </div>
 
