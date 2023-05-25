9156d7730eaa7f9badcbe6a9ad7fa2f05ba30e84
Jea Suson <susonjea@gmail.com>
2023-05-25 21:15:29 +0800

    add users page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,15 @@
 'use client'
 
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
+    const router = useRouter()
+
+    const loginBtn = () => {
+        router.push(`/users/login`)
+    }
+
     return (
         <div className="fixed top-0 w-full z-30 clearNav md:bg-opacity-90 transition duration-300 ease-in-out">
             <div className="flex flex-col max-w-6xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
@@ -17,14 +22,14 @@ const Header = () => {
                             Company Inc.
                         </h1>
                     </Link>
-                    <div className="hidden"><button className='hidden black_btn '>
+                    <button onClick={loginBtn} className='md:hidden black_btn'>
                         Sign in
-                    </button></div>
-                    
+                    </button>
+
                 </div>
-                <div className="hidden flex flex-grow justify-end flex-wrap items-center">
-                    <button className='black_btn'>
-                        Sign in </button>
+                <div className="md:flex hidden flex flex-grow justify-end flex-wrap items-center">
+                    <button onClick={loginBtn} className='black_btn'>
+                    Sign in </button>
                 </div>
 
             </div>
